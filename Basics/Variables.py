25269c230968ec69e64e5d63b616046481f30eab
fazlerabbi13 <mohammad0132794@gmail.com>
2026-06-08 01:55:10 +0600

    Concept of Object Reference.how garbage value is create.

--- Basics/Variables.py
+++ Basics/Variables.py
@@ -1,6 +1,6 @@
-n = 10
-name = "Fazle Rabbi"
-print(n, name)
+# n = 10
+# name = "Fazle Rabbi"
+# print(n, name)
 
 
 # Assigning Same Value
@@ -9,5 +9,16 @@ print(n, name)
 
 
 #  Assigning Different Values
-x, y, z = 7, 3.5,"FR"
-print(x, y, z)
+# x, y, z = 7, 3.5,"FR"
+# print(x, y, z)
+
+
+# Concept of Object Reference(start)
+x = 5
+y= x
+x = "Fazle Rabbi"
+y = "Hello Python"
+
+# now 5 is garbage value
+print(x, "to", y)
+# (end)
