d28482190abd458a892453a5d3dfa3af3bfb7ea3
fazlerabbi13 <mohammad0132794@gmail.com>
2026-06-07 13:42:48 +0600

    checking conditions

--- Basics/conditions.py
+++ Basics/conditions.py
@@ -1,4 +1,4 @@
-# age = int(input("Enter age: "))
+age = int(input("Enter age: "))
 # if age <= 20:
 #     print("eligible for playing")
 # else:
@@ -22,28 +22,28 @@
 
 # age = int(input("Enter age: "))
 # age = 15
-# is_eligible = True
+is_eligible = True
 
-# if age >= 20:
-#     if is_eligible:
-#         print("eligible for vote")
-#     else:
-#         print("not eligible")
-# else:
-#     print("Child")
+if age >= 20:
+    if is_eligible:
+        print("eligible for vote")
+    else:
+        print("not eligible")
+else:
+    print("Child")
 
-# age = int(input("Enter age: "))
-# result = "Adult" if age >= 18 else "child"
-# print(result)
+age = int(input("Enter age: "))
+result = "Adult" if age >= 18 else "child"
+print(result)
 
 # Match-Case Statement
 
-number = int(input("Enter number: "))
-match number:
-    case 1:
-        print("one")
-    case 2 | 3 | 5:
-        print("Two or Three or five")
-    case _:
-        print("other number")
-  
+# number = int(input("Enter number: "))
+# match number:
+#     case 1:
+#         print("one")
+#     case 2 | 3 | 5:
+#         print("Two or Three or five")
+#     case _:
+#         print("other number")
+#
