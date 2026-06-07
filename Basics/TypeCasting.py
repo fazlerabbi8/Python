1b8b53ce1357ff8a5c8c3e492ea2a39cc6df2d2b
fazlerabbi13 <mohammad0132794@gmail.com>
2026-06-08 02:07:12 +0600

    Implicit and Explicit  Type Conversion

---
+++ Basics/TypeCasting.py
@@ -0,0 +1,19 @@
+# Type Casting
+
+x= 7
+y = 2.0
+z = "9"
+
+# casting here
+m = str(x)
+n = int(y)
+k = float(z)
+
+print(m)
+print(n)
+print(k)
+
+
+# print(type(m))
+# print(type(n))
+# print(type(k))
