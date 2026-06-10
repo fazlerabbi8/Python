4434b940b21295b2da10438916bdf38912420958
fazlerabbi13 <mohammad0132794@gmail.com>
2026-06-10 23:42:02 +0600

    multiple conditions

---
+++ Basics/ConditionalStatements.py
@@ -0,0 +1,10 @@
+age = int(input("What is your age? "))
+
+if age <= 15:
+    print("child")
+elif age <= 19 :
+    print("Teenager")
+elif age <= 25:
+    print("Younger")
+else:
+    print("Adult")
