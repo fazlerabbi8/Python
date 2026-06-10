07dc2418b39bcf2a60127caa042c9e0ec88e32ae
fazlerabbi13 <mohammad0132794@gmail.com>
2026-06-10 23:51:23 +0600

    checking largest number

--- Basics/ConditionalStatements.py
+++ Basics/ConditionalStatements.py
@@ -12,6 +12,22 @@
 
 
 # Conditional Expression (Ternary Operator)
-age = int(input("What is your age? "))
-s = "voter" if age >= 20 else "not voter"
-print(s)
+# age = int(input("What is your age? "))
+# s = "voter" if age >= 20 else "not voter"
+# print(s)
+
+
+# Multiple Conditions with If Statement
+
+a  = int(input("number 1: "))
+b = int(input("number 2: "))
+c = int(input("number 3: "))
+
+if a > b and a > c:
+    print("a is largest")
+elif b > a and b > c:
+    print("b is largest")
+elif c > a and c > b:
+    print("c is largest")
+else:
+    print("all numbers are equal")
