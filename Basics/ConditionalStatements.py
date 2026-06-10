f346a57f09da8cfd5f3debdf62b84e0a50884393
fazlerabbi13 <mohammad0132794@gmail.com>
2026-06-10 23:46:31 +0600

    ternary operator

--- Basics/ConditionalStatements.py
+++ Basics/ConditionalStatements.py
@@ -1,10 +1,17 @@
-age = int(input("What is your age? "))
+# age = int(input("What is your age? "))
+#
+# if age <= 15:
+#     print("child")
+# elif age <= 19 :
+#     print("Teenager")
+# elif age <= 25:
+#     print("Younger")
+# else:
+#     print("Adult")
+#
 
-if age <= 15:
-    print("child")
-elif age <= 19 :
-    print("Teenager")
-elif age <= 25:
-    print("Younger")
-else:
-    print("Adult")
+
+# Conditional Expression (Ternary Operator)
+age = int(input("What is your age? "))
+s = "voter" if age >= 20 else "not voter"
+print(s)
