fdaf10379fe074ee809df9eea99fd8e692f9ff8c
fazlerabbi13 <mohammad0132794@gmail.com>
2026-06-10 23:55:35 +0600

    Match-Case Statement

--- Basics/ConditionalStatements.py
+++ Basics/ConditionalStatements.py
@@ -17,17 +17,31 @@
 # print(s)
 
 
-# Multiple Conditions with If Statement
+# Multiple Conditions with If Statement(start)
 
-a  = int(input("number 1: "))
-b = int(input("number 2: "))
-c = int(input("number 3: "))
+# a  = int(input("number 1: "))
+# b = int(input("number 2: "))
+# c = int(input("number 3: "))
+#
+# if a > b and a > c:
+#     print("a is largest")
+# elif b > a and b > c:
+#     print("b is largest")
+# elif c > a and c > b:
+#     print("c is largest")
+# else:
+#     print("all numbers are equal")
 
-if a > b and a > c:
-    print("a is largest")
-elif b > a and b > c:
-    print("b is largest")
-elif c > a and c > b:
-    print("c is largest")
-else:
-    print("all numbers are equal")
+# (end)
+
+# Match-Case Statement
+
+number = int(input("enter a number: "))
+
+match number:
+    case 1:
+        print("print one")
+    case 2:
+        print("print 2")
+    case _:
+        print("others number")
