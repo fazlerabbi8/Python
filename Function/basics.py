12effdf77684d5a434b3e503cf29699b8d1c1c5f
fazlerabbi13 <mohammad0132794@gmail.com>
2026-06-11 23:15:31 +0600

    finding even and odd

---
+++ Function/basics.py
@@ -0,0 +1,13 @@
+# def greetings(text):
+#     print(text)
+#
+# greetings("Hello")
+
+def findEvenOdd(n):
+    if n % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+findEvenOdd(5)
+findEvenOdd(4)
