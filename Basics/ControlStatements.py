4acde7bdc4af07ccddf1fa4a07159fecf11277fc
fazlerabbi13 <mohammad0132794@gmail.com>
2026-06-12 15:46:30 +0600

    loop through in a matrix and finding the desire value.

--- Basics/ControlStatements.py
+++ Basics/ControlStatements.py
@@ -1,9 +1,42 @@
-nums = [4, 5, 7, 2, 10, 3]
-value = 8
-# value = 7
-for i in nums:
-    if i == value:
-        print("number found.")
-        break
-else:
-    print("number not found.")
+# nums = [4, 5, 7, 2, 10, 3]
+# value = 8
+# # value = 7
+# for i in nums:
+#     if i == value:
+#         print("number found.")
+#         break
+# else:
+#     print("number not found.")
+
+
+
+# decrement = 5
+# while True:
+#     print(decrement)
+#     decrement -= 1
+#     if decrement == 0:
+#         print("condition is matched")
+#         break
+
+
+
+
+
+matrix = [
+    [5, 7, 9],
+    [8, 3, 2],
+    [1, 5, 6]
+]
+
+value = 55
+found = False
+
+for i in matrix:
+    for x in i:
+        if x == value:
+            print("Found")
+            found = True
+            break
+if not found:
+    print("Not Found")
+
