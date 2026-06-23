51240f1f0e13de7c56c7530f0861e670fd5c894f
fazlerabbi13 <mohammad0132794@gmail.com>
2026-06-23 12:25:40 +0600

    Adding and Updating Dictionary Items
    Signed-off-by: fazlerabbi13 <mohammad0132794@gmail.com>

--- DataStructure/Dictionary.py
+++ DataStructure/Dictionary.py
@@ -1,5 +1,11 @@
 d = {"name" : "Rabbi", "age" : 22,}
 # print(d)
 
-print(d["age"])
-print(d.get("name"))
+# print(d["age"])
+# print(d.get("name"))
+
+
+d2 = {"name": "ali"}
+d2["age"] = 22
+d2["name"] = "sakib"
+print(d2)
