772a541611d86d8eca85e97e82aed086f2138ba2
fazlerabbi13 <mohammad0132794@gmail.com>
2026-06-23 12:30:15 +0600

    Iterating Through a Dictionary
    Signed-off-by: fazlerabbi13 <mohammad0132794@gmail.com>

--- DataStructure/Dictionary.py
+++ DataStructure/Dictionary.py
@@ -5,7 +5,24 @@ d = {"name" : "Rabbi", "age" : 22,}
 # print(d.get("name"))
 
 
-d2 = {"name": "ali"}
-d2["age"] = 22
-d2["name"] = "sakib"
-print(d2)
+# d2 = {"name": "ali"}
+# d2["age"] = 22
+# d2["name"] = "sakib"
+# print(d2)
+
+
+# d = {"a": 1, "b": 2}
+#
+# val = d.pop("a")
+# print(d)
+
+# Iterating Through a Dictionary
+
+for key in d:
+    print(key)
+
+for value in d.values():
+    print(value)
+
+for key, value in d.items():
+    print(key, value)
