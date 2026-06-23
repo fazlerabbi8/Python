a711379dd5e4c4cc7b6dab1d65eb69ead7e8ea88
fazlerabbi13 <mohammad0132794@gmail.com>
2026-06-23 10:53:16 +0600

    replace word from string using replace() method.
    Signed-off-by: fazlerabbi13 <mohammad0132794@gmail.com>

--- DataStructure/string.py
+++ DataStructure/string.py
@@ -1,9 +1,14 @@
 str = "Hello World"
-print(str[2])
-print(str[-3])
+# print(str[2])
+# print(str[-3])
 
 # String Slicing
-print(str[2:4])
-print(str[:3])
-print(str[3:])
-print(str[::-1])
+# print(str[2:4])
+# print(str[:3])
+# print(str[3:])
+# print(str[::-1])
+
+
+# Updating a String
+str2 = str.replace("World", "Rabbi")
+print(str2)
