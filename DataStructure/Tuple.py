71918ee1bb0c238253e55bd89b7ded8207cae605
fazlerabbi13 <mohammad0132794@gmail.com>
2026-06-23 11:57:45 +0600

    Concat of Tuples
    Signed-off-by: fazlerabbi13 <mohammad0132794@gmail.com>

--- DataStructure/Tuple.py
+++ DataStructure/Tuple.py
@@ -1,2 +1,5 @@
-tup = (7, 'rabbi', 5.5, False, [1 , 2, 3], {'key':'value'})
-print(tup)
+tup1 = (7, 'rabbi', 5.5, False, [1 , 2, 3], {'key':'value'})
+# print(tup)
+tup2 = (1,4,7)
+tup3 = tup1 + tup2
+print(tup3)
