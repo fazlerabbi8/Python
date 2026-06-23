a1ac261d36c8f6408d5f4a8ac955c1c0bb0efac9
fazlerabbi13 <mohammad0132794@gmail.com>
2026-06-23 12:00:24 +0600

    *operator is used in tuple unpacking to grab multiple items into a list.
    Signed-off-by: fazlerabbi13 <mohammad0132794@gmail.com>

--- DataStructure/Tuple.py
+++ DataStructure/Tuple.py
@@ -1,5 +1,11 @@
 tup1 = (7, 'rabbi', 5.5, False, [1 , 2, 3], {'key':'value'})
 # print(tup)
-tup2 = (1,4,7)
-tup3 = tup1 + tup2
-print(tup3)
+# tup2 = (1,4,7)
+# tup3 = tup1 + tup2
+# print(tup3)
+
+
+tup4 = ("rabbi", "ali", "sakib")
+*a , b = tup4
+print(a)
+print(b)
